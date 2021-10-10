c3b1b8bb4f7aa3f1ba2aa1a863af099e45fb73f1
laplamgor <laplamgor@gmail.com>
2021-10-10 20:34:38 +0800

    Add a minimal working version

---
+++ index.es
@@ -0,0 +1,31 @@
+import electron from 'electron';
+
+const { session } = electron.remote
+const path = require('path');
+
+
+// Only block the main.js when the request has version number query (by original code)
+// Do not block the main.js when the request has no version number query (by patcher)
+const filter = { urls: ['http://*/kcs2/js/main.js*'] };
+
+
+
+export const
+    pluginDidLoad = (e) => {
+        console.log('Kantai3D plugin loaded');
+
+        session.defaultSession.webRequest.onBeforeRequest(filter,
+            function (info, c) {
+                console.log('Kantai3D replacing main.js');
+                // The code here is to replace the main.js
+                c({ redirectURL: 'k3d://main.js' })
+                return { redirectURL: 'k3d://main.js' };
+            });
+
+        session.defaultSession.protocol.interceptFileProtocol('k3d', (request, callback) => {
+            callback({ path: path.join(__dirname, "main.js") })
+        })
+    },
+    pluginWillUnload = (e) => {
+        session.defaultSession.webRequest.onBeforeRequest(filter, null);
+    };
---
+++ main.js
@@ -0,0 +1,501 @@
+var xhr = new XMLHttpRequest();
+xhr.open("GET", window.location.protocol + '//' + window.location.hostname + '/kcs2/js/main.js', true);
+xhr.onreadystatechange = () =>
+{
+    if (xhr.readyState === XMLHttpRequest.DONE)
+    {
+        if (xhr.status === 200)
+        {
+            result = xhr.responseText;
+
+
+            // patch the main.js and then store it into the storage
+            patchedMainJs = patchMainJs(result);
+
+            // Better performance than using eval
+            var f = Function(patchedMainJs + ";KCS.init();")();
+        }
+    }
+};
+xhr.send();
+
+
+
+function patchMainJs(contents) {
+
+    contents = contents.toString();
+
+    contents = contents.replace(/(return .{0,99}\!\=.{0,99}\|\|null\!\=\(.{0,99}\=.{0,99}\(.{0,99}\)\)&&\(.{0,99}\='_'\+.{0,99}\),.{0,99}\+\(.{0,99}\+.{0,99}\+'\/'\+\(.{0,99}\+.{0,99}\(.{0,99},.{0,99}\)\)\+'_'\+.{0,99}\+.{0,99}\+.{0,99}\+.{0,99}\(0x0,parseInt\(.{0,99}\)\)\);)/g, 
+        "\n return window.displacementPath = (function () {\n$1\n})();\n");
+
+
+    contents = contents.replace(/(new PIXI\[.{0,99}\]\(.{0,99}\[.{0,99}\]\[.{0,99}\],.{0,99},.{0,99}\);document)/g, 
+        "\n window.pixiApp = $1");
+
+    contents = contents.replace(/(\=[^=]{0,99}\[[^\[]{0,99}\]\[[^\[]{0,99}\]\([^\(]{0,99}\),[^,]{0,99}\=0x0\=\=.{0,99}\?0x0\:.{0,99},.{0,99}\=.{0,99}\[.{0,99}\]\[.{0,99}\]\[.{0,99}\]\(.{0,99}\);)/g, 
+        "\n = window.charar $1");
+
+    contents = contents.replace(/(var .{0,99}\=new PIXI\[\(.{0,99}\)\]\(.{0,99}\);this\[.{0,99}\]\=.{0,99}\[.{0,99}\]\[.{0,99}\]\[.{0,99}\]\[.{0,99}\]\[.{0,99}\]\(.{0,99}\),this\[.{0,99}\]\[.{0,99}]\[.{0,99}\]\(.{0,99},\-.{0,99}\);var [^=]{0,99}=)/g, 
+        "$1 window.charal = \n");
+
+    contents = contents.replace(/(\=[^=]{0,99}\[[^=]{0,99}\]\[[^=]{0,99}\]\[.{0,99}\]\[.{0,99}\]\(.{0,99}\)\[.{0,99}\]\(.{0,99}\);this\[.{0,99}\]\[.{0,99}\]\[.{0,99}\]\(\-.{0,99}\+.{0,99}\['x'\]\+.{0,99},\-.{0,99}\+.{0,99}\['y'\]\),)/g, 
+        "\n = window.charah $1");
+
+    contents = contents.replace(/(\['y'\]\),this\[.{0,99}\('.{0,99}'\)\]\[.{0,99}\('.{0,99}'\)\]\(.{0,99}\+.{0,99},.{0,99}\-.{0,99}\);)/g, 
+        "$1 " + `
+window.portOffset = -window.charal + window.charah.x;//-l+h.x
+window.portOffsetR = window.charar;//r
+
+window.displacementSprite = PIXI.Sprite.fromImage(window.displacementPath.replace(/resources\\/ship\\/full[_dmg]*\\/([0-9]*)_([0-9_a-z]*).png(\\?version=)?([0-9]*)/g, \"https://cdn.jsdelivr.net/gh/laplamgor/kantai3d-depth-maps@master/source/$$1/$$1_$$2_v$$4.png\"));
+window.displacementFilter = PIXI.DepthPerspectiveFilter;
+
+window.displacementFilter.uniforms.textureWidth = this._chara.texture.width;
+window.displacementFilter.uniforms.textureHeight = this._chara.texture.height;
+
+window.displacementSprite.visible = false;
+
+window.displacementFilter.padding = 150;
+
+window.currentChara = this._chara;
+
+if (window.displacementSprite.width != 1) {
+    // The depth map is already loaded
+    window.displacementFilter.uniforms.displacementMap = window.displacementSprite.texture;
+    window.displacementFilter.uniforms.scale = 1.0;
+    window.displacementFilter.uniforms.focus = 0.5;
+    window.displacementFilter.uniforms.offset = [0.0, 0.0];
+    window.currentChara.filters = [window.displacementFilter];
+    window.currentChara.addChild(window.displacementSprite);
+
+    window.mousex1 = null;
+    window.mousey1 = null;
+    prepareJiggle(window.currentChara.texture, window.displacementSprite.texture);
+    window.displacementFilter.uniforms.displacementMap = window.jiggledDepthMapRT.texture;
+} else {
+    // The depth map is not loaded yet, and may not exist in server at all
+    // Disable the filter first
+    this._chara.filters = [];
+    window.currentChara.filters = [];
+    window.displacementSprite.texture.baseTexture.on('loaded', function(){
+        // Re-enable the filter when resource loaded
+        window.displacementFilter.uniforms.displacementMap = window.displacementSprite.texture;
+        window.displacementFilter.uniforms.scale = 1.0;
+        window.displacementFilter.uniforms.focus = 0.5;
+        window.displacementFilter.uniforms.offset = [0.0, 0.0];
+        window.currentChara.filters = [window.displacementFilter];
+        window.currentChara.addChild(window.displacementSprite);
+
+        window.mousex1 = null;
+        window.mousey1 = null;
+        prepareJiggle(window.currentChara.texture, window.displacementSprite.texture);
+        window.displacementFilter.uniforms.displacementMap = window.jiggledDepthMapRT.texture;
+    });
+}
+
+
+///////////////////////////////////
+
+function prepareJiggle(baseMap, depthMap) {
+
+    window.jigglePositions = [];
+    window.jiggleVelocities = [];
+    window.jiggleForces = [];
+
+    window.jiggleStaticFlags = [];
+    window.jiggleMovement = [];
+
+    window.damping = [];//1.0 / 8; // 1 2 4 8 16 
+    window.springiness = [];//1.0 / 16.0; // 0 2 4 8 16 32 回彈力
+    
+
+    var depthImg = depthMap.baseTexture.source;
+    var tempCanvas = document.createElement('canvas');
+    tempCanvas.width = depthImg.width;
+    tempCanvas.height = depthImg.height;
+    let tmCtx = tempCanvas.getContext('2d');
+    tmCtx.drawImage(depthImg, 0, 0);
+    var dmData = tmCtx.getImageData(0, 0, depthImg.width, depthImg.height).data;
+
+
+    window.jiggleMeshW = Math.ceil(baseMap.width / 10.0);
+    window.jiggleMeshH = Math.ceil(baseMap.height / 10.0);
+
+    // This is the jiggled mseh
+    window.jiggledDepthMapMesh = new PIXI.mesh.Plane(window.displacementSprite.texture, window.jiggleMeshW, window.jiggleMeshH);
+    window.jiggledDepthMapMesh.visible = false;
+
+    // This is the render texture of the jiggled mseh
+    window.jiggledDepthMapRT = new PIXI.Sprite(PIXI.RenderTexture.create(baseMap.width, baseMap.height));
+    window.jiggledDepthMapRT.visible = false;
+
+    
+    window.jiggledBaseMapMesh = new PIXI.mesh.Plane(baseMap, window.jiggleMeshW, window.jiggleMeshH);
+    
+    window.pixiApp.stage.addChild(window.jiggledDepthMapMesh);
+    window.pixiApp.stage.addChild(window.jiggledDepthMapRT);
+    window.currentChara.addChild(window.jiggledBaseMapMesh);
+    
+    window.gridW = baseMap.width / (window.jiggleMeshW - 1.0);
+    window.gridH = baseMap.height / (window.jiggleMeshH - 1.0);
+    for (var y = 0; y < window.jiggleMeshH; y++) {
+        for (var x = 0; x < window.jiggleMeshW; x++) {
+            window.jigglePositions.push({ x: window.gridW * x, y: y * window.gridH });
+            window.jiggleVelocities.push({ x: 0, y: 0 });
+            window.jiggleForces.push({ x: 0, y: 0 });
+
+            var r = dmData[(Math.floor(y * window.gridH) * baseMap.width + Math.floor(x * window.gridW)) * 4 + 0];
+            var b = dmData[(Math.floor(y * window.gridH) * baseMap.width + Math.floor(x * window.gridW)) * 4 + 2];
+
+            window.damping.push(1.0 / (b / 255.0 * 16.0 + 1));//1.0 / 8; // 1 2 4 8 16 
+            window.springiness.push(1.0 / ( b / 255.0 * 32.0 + 1));//1.0 / 16.0; // 0 2 4 8 16 32 回彈力
+        
+
+            window.jiggleStaticFlags.push(b == 0);
+            window.jiggleMovement.push((r - 127.0) / 128.0);
+        }
+    }
+    
+    window.Mx = null;
+    window.My = null;
+    window.Mx2 = null;
+    window.My2 = null;
+}
+`);
+
+    contents = contents.replace(/(\=Math\[.{0,99}\]\(.{0,99}\),.{0,99}\=0x1\+0\.012\*\(0\.5\+0\.5\*.{0,99}\);this\[.{0,99}\]\[.{0,99}\]\(.{0,99}\),)/g, 
+        "\n = window.charasin $1");
+
+    contents = contents.replace(/(this\['y'\]=this\[.{0,99}\('.{0,99}'\)]-1.5\*.{0,99}\*1.8;)/g, 
+        `$1
+
+window.displacementFilter.uniforms.textureScale = this.scale.x;
+
+`);
+
+    return contents + `;
+
+
+    document.addEventListener('mouseleave', e => {
+        window.mouseoutside = true;
+        window.mousex1 = null;
+        window.mousey1 = null;
+    });
+    
+    document.addEventListener('mouseenter', e => {
+        window.mouseoutside = false;
+    });
+    
+    
+    
+    
+    'use strict';
+    PIXI.DepthPerspectiveFilter = new PIXI.Filter(null, \`` + frag + `\`);
+    
+    PIXI.DepthPerspectiveFilter.apply = function(filterManager, input, output)
+    {
+      this.uniforms.dimensions = {};
+      this.uniforms.dimensions[0] = input.sourceFrame.width;
+      this.uniforms.dimensions[1] = input.sourceFrame.height;
+    
+      this.uniforms.padding = this.padding;
+      
+      this.uniforms.frameWidth = input.size.width;
+      this.uniforms.frameHeight = input.size.height;
+    
+    
+      //////// mouse
+      var mousex2 = (window.pixiApp.renderer.plugins.interaction.mouse.global.x);
+      var mousey2 = (window.pixiApp.renderer.plugins.interaction.mouse.global.y);
+    
+      if (!window.mouseoutside) {
+          if (!window.mousex1)
+              window.mousex1 = mousex2;
+          if (!window.mousey1)
+              window.mousey1 = mousey2;
+    
+          if (!window.mousex)
+              window.mousex = 0;
+          if (!window.mousey)
+              window.mousey = 0;
+          if (!window.mouset)
+              window.mouset = Date.now();
+    
+          window.mousex += (mousex2 - window.mousex1) * (Date.now() - window.mouset);
+          window.mousey += (mousey2 - window.mousey1) * (Date.now() - window.mouset);
+    
+          window.mousex1 = mousex2;
+          window.mousey1 = mousey2;
+      }
+    
+      var flip = (window.currentChara.transform.position.x - window.portOffset) / (window.portOffsetR) - 0.5;
+      window.displacementFilter.uniforms.offset = [flip * (window.mousex / 1200.0 * 0.05) * 1.3, 0.02 * window.charasin - 0.01 + (window.mousey / 720.0 * 0.05) * 0.6];
+    
+      window.mousex = window.mousex * Math.pow(0.9, (Date.now() - window.mouset) / 30.0);
+      window.mousey = window.mousey * Math.pow(0.9, (Date.now() - window.mouset) / 30.0);
+      window.mouset = Date.now();
+    
+    
+      ////////
+      
+      var vertices = window.jiggledBaseMapMesh.vertices;
+      var vertices2 = window.jiggledDepthMapMesh.vertices;
+    
+      var newMx = window.displacementFilter.uniforms.offset[0];
+      var newMy = window.displacementFilter.uniforms.offset[1];
+      
+      var baseMap = window.currentChara.texture;
+      var depthMap = window.displacementSprite.texture;
+      if (baseMap && baseMap.baseTexture && depthMap && depthMap.baseTexture) {
+    
+          window.My2 = window.My;
+          window.Mx2 = window.Mx;
+          window.My = newMy;
+          window.Mx = newMx;
+          for (var y = 0; y < window.jiggleMeshH; y++) {
+              for (var x = 0; x < window.jiggleMeshW; x++) {
+                  resetForce(x, y);
+              }
+          }
+    
+          if (window.Mx && window.My && window.Mx2 && window.My2 && newMx != -999999 && window.Mx != -999999 && window.Mx2 != -999999) {
+      
+              var aX = (window.Mx2 - window.Mx) - (window.Mx - newMx);
+              var aY = (window.My2 - window.My) - (window.My - newMy);
+      
+              for (var y = 0; y < window.jiggleMeshH; y++) {
+                  for (var x = 0; x < window.jiggleMeshW; x++) {
+                      var m = window.jiggleMovement[y * window.jiggleMeshW + x];
+                      window.jiggleForces[y * window.jiggleMeshW + x].x += aX * m * -50;
+                      window.jiggleForces[y * window.jiggleMeshW + x].y += aY * m * 50;
+                  }
+              }
+          }
+      
+    
+          for (var y = 0; y < window.jiggleMeshH; y++) {
+              for (var x = 0; x < window.jiggleMeshW; x++) {
+                  if (x != 0) {
+                      springUpdate(x - 1, y, x, y);
+                  }
+                  if (y != 0) {
+                      springUpdate(x, y - 1, x, y);
+                  }
+              }
+          }
+      
+      
+          for (var y = 0; y < window.jiggleMeshH; y++) {
+              for (var x = 0; x < window.jiggleMeshW; x++) {
+                  addDampingForce(x, y);
+                  update(x, y);
+              }
+          }
+    
+      
+          for (var i = 0; i < window.jigglePositions.length; i++) {
+              var pos = window.jigglePositions[i];
+              vertices[i * 2] = Math.min(Math.max(pos.x, 0), baseMap.width);
+              vertices[i * 2 + 1] = Math.min(Math.max(pos.y, 0), baseMap.height);
+      
+              vertices2[i * 2] = vertices[i * 2];
+              vertices2[i * 2 + 1] = vertices[i * 2 + 1];
+          }
+      }
+      ////////
+    
+    
+    
+    
+    
+    
+    
+    
+    
+      window.jiggledDepthMapMesh.visible = true;
+      window.pixiApp.renderer.render(window.jiggledDepthMapMesh, window.jiggledDepthMapRT.texture);
+      window.jiggledDepthMapMesh.visible = false;
+    
+    
+      // draw the filter...
+      filterManager.applyFilter(this, input, output);
+    }
+    
+    
+    function resetForce(x, y) {
+        window.jiggleForces[y * window.jiggleMeshW + x] = { x: 0, y: 0 };
+    }
+    
+    function addForce(x, y, addX, addY) {
+        var f = window.jiggleForces[y * window.jiggleMeshW + x];
+        f.x += addX;
+        f.y += addY;
+    }
+    
+    function addDampingForce(x, y) {
+        var v = jiggleVelocities[y * window.jiggleMeshW + x];
+        var f = window.jiggleForces[y * window.jiggleMeshW + x];
+        f.x -= v.x * window.damping[y * window.jiggleMeshW + x];
+        f.y -= v.y * window.damping[y * window.jiggleMeshW + x];
+    }
+    
+    
+    function update(x, y) {
+        var p = window.jigglePositions[y * window.jiggleMeshW + x];
+        var v = window.jiggleVelocities[y * window.jiggleMeshW + x];
+        var f = window.jiggleForces[y * window.jiggleMeshW + x];
+    
+        if (window.jiggleStaticFlags[y * window.jiggleMeshW + x] == false) {
+            v.x += f.x;
+            v.y += f.y;
+            p.x += v.x;
+            p.y += v.y;
+        }
+    }
+    
+    
+    
+    function springUpdate(x1, y1, x2, y2) {
+        if (window.jiggleStaticFlags[x1 + y1 * window.jiggleMeshW.w] && window.jiggleStaticFlags[x2 + y2 * window.jiggleMeshW.w]) 
+            return;
+    
+        var distanceOrigin = (x2 - x1) * window.gridW + (y2 - y1) * window.gridH;
+        
+        
+    
+        var p1 = window.jigglePositions[y1 * window.jiggleMeshW + x1];
+        var p2 = window.jigglePositions[y2 * window.jiggleMeshW + x2];
+    
+        var distance = len(sub(p1, p2));
+    
+        var springiness = (window.springiness[y1 * window.jiggleMeshW + x1] + window.springiness[y2 * window.jiggleMeshW + x2]) / 2;
+    
+        var springForce = springiness * (distanceOrigin - distance);
+        var frcToAdd = tim(normalize(sub(p1, p2)), springForce);
+    
+        addForce(x1, y1, frcToAdd.x, frcToAdd.y);
+        addForce(x2, y2, -frcToAdd.x, -frcToAdd.y);
+    }
+    
+    
+    function len(v) {
+        return Math.sqrt(v.x * v.x + v.y * v.y);
+    }
+    
+    function normalize(v) {
+        var l = len(v);
+        return { x: v.x / l, y: v.y / l };
+    }
+    
+    function sub(v1, v2) {
+        return { x: v1.x - v2.x, y: v1.y - v2.y }
+    }
+    
+    function tim(v1, s) {
+        return { x: v1.x * s, y: v1.y * s }
+    }
+`;
+}
+
+let frag = `precision mediump float;
+uniform vec2 offset;
+
+
+uniform sampler2D uSampler;
+uniform sampler2D displacementMap;
+
+uniform float textureScale;
+
+uniform float textureWidth;
+uniform float textureHeight;
+uniform float frameWidth;
+uniform float frameHeight;
+
+uniform float padding;
+uniform vec4 filterArea;
+uniform vec4 filterClamp;
+
+
+varying vec2 vTextureCoord;
+varying vec4 vColor;
+uniform vec4 dimensions;
+uniform vec2 mapDimensions;
+uniform float scale;
+uniform float focus;
+
+
+vec2 mapCoordDepth(vec2 coord)
+{
+    return vec2((coord[0] * frameWidth - padding) / textureWidth / textureScale,
+                (coord[1] * frameHeight - padding) / textureHeight / textureScale);
+}
+
+vec2 mapCoord2(vec2 coord)
+{
+    return vec2(coord[0] * frameWidth / textureWidth / textureScale,
+                coord[1] * frameHeight / textureHeight / textureScale);
+}
+
+const float compression = 1.0;
+const float dmin = 0.0;
+const float dmax = 1.0;
+
+#define MAXSTEPS 600.0
+float steps = max(MAXSTEPS *length(offset), 30.0);
+
+
+
+void main(void)
+{
+
+    float aspect = dimensions.x / dimensions.y;
+    vec2 scale2 =
+        vec2(scale * min(1.0, 1.0 / aspect), scale * min(1.0, aspect)) * vec2(1, -1);
+    mat2 baseVector =
+        mat2(vec2((0.5 - focus) * (offset) - (offset) / 2.0) * scale2,
+             vec2((0.5 - focus) * (offset) + (offset) / 2.0) * scale2);
+
+    vec2 pos = (vTextureCoord);
+    mat2 vector = baseVector;
+
+    float dstep = compression / (steps - 1.0);
+    vec2 vstep = (vector[1] - vector[0]) / vec2((steps - 1.0));
+
+    vec2 posSumLast = vec2(0.0);
+    vec2 posSum = vec2(0.0);
+
+    float weigth = 1.0;
+    float dpos;
+    float dposLast;
+
+    for (float i = 0.0; i < MAXSTEPS; ++i)
+    {
+        vec2 vpos = pos + vector[1] - i * vstep;
+        dpos = 1.0 - i * dstep;
+        float depth = texture2D(displacementMap, mapCoordDepth(vpos)).r;
+
+
+        if (texture2D(uSampler, vpos)[3] == 0.0)
+        {
+            depth = 0.0;
+        }
+
+        depth = clamp(depth, dmin, dmax);
+
+        if (dpos > depth)
+        {
+            posSumLast = vpos;
+            dposLast = dpos;
+        }
+        else
+        {
+            posSum = vpos;
+            weigth = (depth - dposLast) / dstep;
+            break;
+        }
+    };
+
+    gl_FragColor = texture2D(
+                       uSampler,
+                       (posSum - posSumLast) * -clamp(weigth * 0.5 + 0.5, 0.0, 1.5) + posSum);
+
+}`;
